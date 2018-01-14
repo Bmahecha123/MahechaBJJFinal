1e84d59bc1da31aa7169e4a2d51f48f607a646fb
Brian Mahecha <mahebd@mail.broward.edu>
2018-01-13 19:13:28 -0500

    finished playlist view page

--- MahechaBJJ/Views/PlaylistPages/PlaylistViewPage.cs
+++ MahechaBJJ/Views/PlaylistPages/PlaylistViewPage.cs
@@ -24,10 +24,10 @@ namespace MahechaBJJ.Views.PlaylistPages
         private StackLayout layout;
         private Account account;
         private string id;
-		private Label timeOutLbl;
-		private Frame timeOutFrame;
-		private TapGestureRecognizer timeOutTap;
-		private ActivityIndicator activityIndicator;
+        private Label timeOutLbl;
+        private Frame timeOutFrame;
+        private TapGestureRecognizer timeOutTap;
+        private ActivityIndicator activityIndicator;
         private ObservableCollection<PlayList> userPlaylist;
 
         public PlaylistViewPage()
@@ -35,118 +35,124 @@ namespace MahechaBJJ.Views.PlaylistPages
             _playlistViewPageViewModel = new PlaylistViewPageViewModel();
             _baseViewModel = new BaseViewModel();
             Title = "User Playlists";
-            Padding = new Thickness(10, 30, 10, 10);
-			BuildPageObjects();
-			//FindPlaylists();
+#if __IOS__
+                    Padding = new Thickness(10, 30, 10, 10);
+#endif
+#if __ANDROID__
+            Padding = new Thickness(5, 5, 5, 5);
+#endif
+            BuildPageObjects();
+            //FindPlaylists();
             SetContent();
         }
 
         //Functions
         public void BuildPageObjects()
         {
-			var btnSize = Device.GetNamedSize(NamedSize.Large, typeof(Button));
-			var lblSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+            var btnSize = Device.GetNamedSize(NamedSize.Large, typeof(Button));
+            var lblSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
 
-			//View Objects
-			innerGrid = new Grid
-			{
-				RowDefinitions = new RowDefinitionCollection
-				{
-					new RowDefinition { Height = new GridLength(1, GridUnitType.Star)},
-					new RowDefinition { Height = new GridLength(7, GridUnitType.Star)},
-					new RowDefinition { Height = new GridLength(1, GridUnitType.Star)}
-				}
-			};
-			outerGrid = new Grid
-			{
-				RowDefinitions = new RowDefinitionCollection
-				{
-					new RowDefinition { Height = new GridLength(1, GridUnitType.Star)}
-				}
-			};
+            //View Objects
+            innerGrid = new Grid
+            {
+                RowDefinitions = new RowDefinitionCollection
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star)},
+                    new RowDefinition { Height = new GridLength(7, GridUnitType.Star)},
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star)}
+                }
+            };
+            outerGrid = new Grid
+            {
+                RowDefinitions = new RowDefinitionCollection
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star)}
+                }
+            };
 
-			viewPlaylistLbl = new Label
-			{
+            viewPlaylistLbl = new Label
+            {
+#if __IOS__
+				FontFamily = "AmericanTypewriter-Bold",
+                FontSize = lblSize * 2,
+#endif
+#if __ANDROID__
+                FontFamily = "Roboto Bold",
+                FontSize = lblSize,
+#endif
+                Text = "View Playlists",
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            playlistView = new ListView
+            {
+                HasUnevenRows = true,
+                SeparatorVisibility = SeparatorVisibility.None
+            };
+
+            backBtn = new Button
+            {
 #if __IOS__
 				FontFamily = "AmericanTypewriter-Bold",
 #endif
 #if __ANDROID__
                 FontFamily = "Roboto Bold",
 #endif
-				Text = "View Playlists",
-				FontSize = lblSize * 2,
-				VerticalTextAlignment = TextAlignment.Center,
-				HorizontalTextAlignment = TextAlignment.Center
-			};
+                Text = "Back",
+                FontSize = btnSize * 2,
+                BackgroundColor = Color.FromRgb(124, 37, 41),
+                BorderWidth = 3,
+                TextColor = Color.Black
+            };
 
-			playlistView = new ListView
-			{
-				HasUnevenRows = true,
-				SeparatorVisibility = SeparatorVisibility.None
-			};
-
-			backBtn = new Button
-			{
+            timeOutLbl = new Label
+            {
 #if __IOS__
 				FontFamily = "AmericanTypewriter-Bold",
 #endif
 #if __ANDROID__
                 FontFamily = "Roboto Bold",
 #endif
-				Text = "Back",
-				FontSize = btnSize * 2,
-				BackgroundColor = Color.FromRgb(124, 37, 41),
-				BorderWidth = 3,
-				TextColor = Color.Black
-			};
+                Text = "Network Has Timed Out! \n Click To Try Again!",
+                LineBreakMode = LineBreakMode.WordWrap,
+                FontSize = lblSize,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.White
+            };
+            timeOutFrame = new Frame
+            {
+                Content = timeOutLbl,
+                OutlineColor = Color.Black,
+                BackgroundColor = Color.Black,
+                HasShadow = false,
+                Padding = 3,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            timeOutTap = new TapGestureRecognizer();
+            timeOutTap.Tapped += (sender, e) =>
+            {
+                SetContent();
+            };
+            timeOutLbl.GestureRecognizers.Add(timeOutTap);
+            activityIndicator = new ActivityIndicator
+            {
+                IsRunning = false,
+                IsEnabled = true,
+                IsVisible = true
+            };
 
-			timeOutLbl = new Label
-			{
-#if __IOS__
-				FontFamily = "AmericanTypewriter-Bold",
-#endif
-#if __ANDROID__
-                FontFamily = "Roboto Bold",
-#endif
-				Text = "Network Has Timed Out! \n Click To Try Again!",
-				LineBreakMode = LineBreakMode.WordWrap,
-				FontSize = lblSize,
-				VerticalTextAlignment = TextAlignment.Center,
-				HorizontalTextAlignment = TextAlignment.Center,
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				TextColor = Color.White
-			};
-			timeOutFrame = new Frame
-			{
-				Content = timeOutLbl,
-				OutlineColor = Color.Black,
-				BackgroundColor = Color.Black,
-				HasShadow = false,
-				Padding = 3,
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
-				VerticalOptions = LayoutOptions.CenterAndExpand
-			};
-			timeOutTap = new TapGestureRecognizer();
-			timeOutTap.Tapped += (sender, e) =>
-			{
-				SetContent();
-			};
-			timeOutLbl.GestureRecognizers.Add(timeOutTap);
-			activityIndicator = new ActivityIndicator
-			{
-				IsRunning = false,
-				IsEnabled = true,
-				IsVisible = true
-			};
+            //Events
+            backBtn.Clicked += GoBack;
+            playlistView.ItemSelected += LoadPlaylist;
 
-			//Events
-			backBtn.Clicked += GoBack;
-			playlistView.ItemSelected += LoadPlaylist;
+            outerGrid.Children.Add(innerGrid, 0, 0);
 
-			outerGrid.Children.Add(innerGrid, 0, 0);
-
-			Content = outerGrid;
+            Content = outerGrid;
         }
 
         private void SetContent()
@@ -163,17 +169,32 @@ namespace MahechaBJJ.Views.PlaylistPages
             if (_playlistViewPageViewModel.Successful)
             {
 
-				//building grid
-				innerGrid.Children.Clear();
-				innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
-				innerGrid.Children.Add(playlistView, 0, 1);
-				innerGrid.Children.Add(backBtn, 0, 2);
+                //building grid
+                innerGrid.Children.Clear();
+                innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
+#if __ANDROID__
+                innerGrid.Children.Add(playlistView, 0, 1);
+                Grid.SetRowSpan(playlistView, 2);
+#endif
+#if __IOS__
+                innerGrid.Children.Add(playlistView, 0, 1);
+                innerGrid.Children.Add(backBtn, 0, 2);
+#endif
 
-            } else {
-				innerGrid.Children.Clear();
-				innerGrid.Children.Add(timeOutFrame, 0, 0);
+            }
+            else
+            {
+                innerGrid.Children.Clear();
+#if __ANDROID__
+                innerGrid.Children.Add(timeOutFrame, 0, 0);
+                Grid.SetRowSpan(timeOutFrame, 3);
+#endif
+#if __IOS__
+                innerGrid.Children.Add(timeOutFrame, 0, 0);
                 Grid.SetRowSpan(timeOutFrame, 2);
                 innerGrid.Children.Add(backBtn, 0, 2);
+#endif
+
             }
         }
 
@@ -189,15 +210,15 @@ namespace MahechaBJJ.Views.PlaylistPages
             id = account.Properties["Id"];
             await _playlistViewPageViewModel.GetUserPlaylists(Constants.GETPLAYLIST, id);
             userPlaylist = _playlistViewPageViewModel.Playlist;
-			SetListView();
-		}
+            SetListView();
+        }
 
         public void SetListView()
         {
             playlistView.ItemsSource = userPlaylist;
             playlistView.ItemTemplate = new DataTemplate(() =>
             {
-				playlistLbl = new Label();
+                playlistLbl = new Label();
                 playlistLbl.SetBinding(Label.TextProperty, "Name");
                 playlistLbl.VerticalTextAlignment = TextAlignment.Center;
                 playlistLbl.HorizontalTextAlignment = TextAlignment.Center;
@@ -211,11 +232,11 @@ namespace MahechaBJJ.Views.PlaylistPages
                 playlistLbl.FontFamily = "Roboto Bold";
 #endif
 
-				playlistFrame = new Frame();
+                playlistFrame = new Frame();
                 playlistFrame.BackgroundColor = Color.FromRgb(58, 93, 174);
-				playlistFrame.HasShadow = false;
-				playlistFrame.OutlineColor = Color.Black;
-				//playlistFrame.Padding = 3;
+                playlistFrame.HasShadow = false;
+                playlistFrame.OutlineColor = Color.Black;
+                //playlistFrame.Padding = 3;
                 playlistFrame.Content = playlistLbl;
                 playlistFrame.Padding = new Thickness(10, 10, 10, 10);
 
@@ -239,7 +260,7 @@ namespace MahechaBJJ.Views.PlaylistPages
         public void LoadPlaylist(object sender, SelectedItemChangedEventArgs e)
         {
             PlayList playlist = (PlayList)((ListView)sender).SelectedItem;
-            if (e.SelectedItem == null) 
+            if (e.SelectedItem == null)
             {
                 return;
             }
@@ -247,53 +268,78 @@ namespace MahechaBJJ.Views.PlaylistPages
             Navigation.PushModalAsync(new PlaylistDetailPage(playlist));
         }
 
-		//page reloading
-		protected override async void OnAppearing()
-		{
-				base.OnAppearing();
-				PlaylistViewPageViewModel vm = new PlaylistViewPageViewModel();
-				await vm.GetUserPlaylists(Constants.GETPLAYLIST, id);
-				playlistView.ItemsSource = vm.Playlist;
-		}
+        //page reloading
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            PlaylistViewPageViewModel vm = new PlaylistViewPageViewModel();
+            await vm.GetUserPlaylists(Constants.GETPLAYLIST, id);
+            playlistView.ItemsSource = vm.Playlist;
+        }
 
-		//Orientation
-		protected override void OnSizeAllocated(double width, double height)
-		{
-			base.OnSizeAllocated(width, height); //must be called
+        //Orientation
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //must be called
 
-			if (width > height)
-			{
+            if (width > height)
+            {
                 if (_playlistViewPageViewModel.Playlist != null)
                 {
-					Padding = new Thickness(10, 10, 10, 10);
-					innerGrid.RowDefinitions.Clear();
-					innerGrid.ColumnDefinitions.Clear();
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-					innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-					innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-					innerGrid.Children.Clear();
-					innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
-					innerGrid.Children.Add(backBtn, 0, 2);
-					innerGrid.Children.Add(playlistView, 1, 0);
-					Grid.SetRowSpan(playlistView, 3);
+#if __IOS__
+                    Padding = new Thickness(10, 10, 10, 10);
+#endif
+#if __ANDROID__
+                    Padding = new Thickness(5, 5, 5, 5);
+#endif
+                    innerGrid.RowDefinitions.Clear();
+                    innerGrid.ColumnDefinitions.Clear();
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+                    innerGrid.Children.Clear();
+                    innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
+#if __ANDROID__
+                    innerGrid.Children.Add(playlistView, 1, 0);
+                    Grid.SetRowSpan(playlistView, 3);  
+#endif
+#if __IOS__
+            innerGrid.Children.Add(backBtn, 0, 2);
+                    innerGrid.Children.Add(playlistView, 1, 0);
+                    Grid.SetRowSpan(playlistView, 3);
+#endif        
+                    
                 }
-			}
-			else
-			{
+            }
+            else
+            {
                 if (_playlistViewPageViewModel.Playlist != null)
                 {
-					Padding = new Thickness(10, 30, 10, 10);
-					innerGrid.RowDefinitions.Clear();
-					innerGrid.ColumnDefinitions.Clear();
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(7, GridUnitType.Star) });
-					innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-					innerGrid.Children.Clear();
-					innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
-					innerGrid.Children.Add(playlistView, 0, 1);
-					innerGrid.Children.Add(backBtn, 0, 2);
+#if __IOS__
+                    Padding = new Thickness(10, 30, 10, 10);
+#endif
+#if __ANDROID__
+                    Padding = new Thickness(5, 5, 5, 5);
+#endif
+                    innerGrid.RowDefinitions.Clear();
+                    innerGrid.ColumnDefinitions.Clear();
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(7, GridUnitType.Star) });
+                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    innerGrid.Children.Clear();
+#if __IOS__
+                    innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
+                    innerGrid.Children.Add(playlistView, 0, 1);
+                    innerGrid.Children.Add(backBtn, 0, 2);
+#endif
+#if __ANDROID__
+                    innerGrid.Children.Add(viewPlaylistLbl, 0, 0);
+                    innerGrid.Children.Add(playlistView, 0, 1);
+                    Grid.SetRowSpan(playlistView, 2);
+#endif
+
                 }
 			}
 		}
