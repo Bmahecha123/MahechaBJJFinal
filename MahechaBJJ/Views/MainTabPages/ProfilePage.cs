9e52259c1aa999b706ecce172ddda4524266d9bd
Brian Mahecha <mahebd@mail.broward.edu>
2018-02-19 17:26:52 -0500

    finished profile page

--- MahechaBJJ/Views/MainTabPages/ProfilePage.cs
+++ MahechaBJJ/Views/MainTabPages/ProfilePage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MahechaBJJ.Droid;
 using MahechaBJJ.Model;
 using MahechaBJJ.Resources;
 using MahechaBJJ.Service;
@@ -11,6 +12,7 @@ using MahechaBJJ.Views.EntryPages;
 using MahechaBJJ.Views.SignUpPages;
 using Xamarin.Auth;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace MahechaBJJ.Views
 {
@@ -39,6 +41,21 @@ namespace MahechaBJJ.Views
         private StackLayout userCredentialStack;
         private Button createAccountBtn;
         private bool hasAccount;
+#if __ANDROID__
+        private Android.Widget.TextView androidNameLbl;
+        private Android.Widget.TextView androidEmailLbl;
+        private Android.Widget.TextView androidBeltLbl;
+        private Android.Widget.Button androidPackageBtn;
+        private Android.Widget.Button androidContactUsBtn;
+        private Android.Widget.Button androidLogOutBtn;
+        private Android.Widget.Button androidLoginBtn;
+        private Android.Widget.Button androidSettingsBtn;
+        private Android.Widget.Button androidCreateAccountBtn;
+        private ContentView contentViewNameLbl;
+        private ContentView contentViewEmailLbl;
+        private ContentView contentViewBeltLbl;
+
+#endif
 
         public ProfilePage(bool hasAccount)
         {
@@ -326,6 +343,104 @@ namespace MahechaBJJ.Views
                 IsVisible = true
             };
 
+#if __ANDROID__
+            androidNameLbl = new Android.Widget.TextView(MainApplication.ActivityContext);
+            androidNameLbl.Text = "Name:";
+            androidNameLbl.SetTextColor(Android.Graphics.Color.Black);
+            androidNameLbl.Gravity = Android.Views.GravityFlags.Start;
+            androidNameLbl.SetTextSize(Android.Util.ComplexUnitType.Fraction, 100);
+            //androidNameLbl.SetTypeface(androidNameLbl.Typeface, Android.Graphics.TypefaceStyle.Bold);
+
+            androidEmailLbl = new Android.Widget.TextView(MainApplication.ActivityContext);
+            androidEmailLbl.Text = "Email:";
+            androidEmailLbl.SetTextColor(Android.Graphics.Color.Black);
+            androidEmailLbl.Gravity = Android.Views.GravityFlags.Start;
+            androidEmailLbl.SetTextSize(Android.Util.ComplexUnitType.Fraction, 100);
+            //androidEmailLbl.SetTypeface(androidNameLbl.Typeface, Android.Graphics.TypefaceStyle.Bold);
+
+            androidBeltLbl = new Android.Widget.TextView(MainApplication.ActivityContext);
+            androidBeltLbl.Text = "Belt:";
+            androidBeltLbl.SetTextColor(Android.Graphics.Color.Black);
+            androidBeltLbl.Gravity = Android.Views.GravityFlags.Start;
+            androidBeltLbl.SetTextSize(Android.Util.ComplexUnitType.Fraction, 100);
+            //androidBeltLbl.SetTypeface(androidNameLbl.Typeface, Android.Graphics.TypefaceStyle.Bold);
+
+            androidPackageBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidPackageBtn.Text = "Packages";
+            androidPackageBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidPackageBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidPackageBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidPackageBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidPackageBtn.SetAllCaps(false);
+            androidPackageBtn.Click += (object sender, EventArgs e) => {
+                SetPackages();
+            };
+
+            androidContactUsBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidContactUsBtn.Text = "Contact Us";
+            androidContactUsBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidContactUsBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidContactUsBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidContactUsBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidContactUsBtn.SetAllCaps(false);
+            androidContactUsBtn.Click += (object sender, EventArgs e) => {
+                ContactUs();
+            };
+
+            androidLogOutBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidLogOutBtn.Text = "Log Out";
+            androidLogOutBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidLogOutBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidLogOutBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidLogOutBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidLogOutBtn.SetAllCaps(false);
+            androidLogOutBtn.Click += async (object sender, EventArgs e) => {
+                await LogOutClick();
+            };
+
+            androidLoginBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidLoginBtn.Text = "Login";
+            androidLoginBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidLoginBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidLoginBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidLoginBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidLoginBtn.SetAllCaps(false);
+            androidLoginBtn.Click += (sender, e) => {
+                Navigation.PushModalAsync(new LoginPage());
+            };
+
+            androidSettingsBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidSettingsBtn.Text = "Settings";
+            androidSettingsBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidSettingsBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidSettingsBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidSettingsBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidSettingsBtn.SetAllCaps(false);
+            androidSettingsBtn.Click += async (object sender, EventArgs e) => {
+                await Settings();
+            };
+
+            androidCreateAccountBtn = new Android.Widget.Button(MainApplication.ActivityContext);
+            androidCreateAccountBtn.Text = "Create Account";
+            androidCreateAccountBtn.SetAutoSizeTextTypeWithDefaults(Android.Widget.AutoSizeTextType.Uniform);
+            androidCreateAccountBtn.SetBackgroundColor(Android.Graphics.Color.Rgb(58, 93, 174));
+            androidCreateAccountBtn.SetTextColor(Android.Graphics.Color.Black);
+            androidCreateAccountBtn.Gravity = Android.Views.GravityFlags.Center;
+            androidCreateAccountBtn.SetAllCaps(false);
+            androidCreateAccountBtn.Click += (object sender, EventArgs e) => {
+                Navigation.PushModalAsync(new SignUpPage());
+            };
+
+            contentViewNameLbl = new ContentView();
+            contentViewNameLbl.Content = androidNameLbl.ToView();
+
+            contentViewEmailLbl = new ContentView();
+            contentViewEmailLbl.Content = androidEmailLbl.ToView();
+
+            contentViewBeltLbl = new ContentView();
+            contentViewBeltLbl.Content = androidBeltLbl.ToView();
+#endif
+
             //Events
             packageBtn.Clicked += (object sender, EventArgs e) =>
             {
@@ -333,34 +448,15 @@ namespace MahechaBJJ.Views
             };
             contactUsBtn.Clicked += (sender, e) =>
             {
-                contactUsBtn.IsEnabled = false;
-                var emailMessage = new EmailMessage();
-                emailMessage.Subject = "Mahecha BJJ - <Insert Subject Here>";
-#if __IOS__
-                MessagingCenter.Send(this, "Send EMail", emailMessage);
-#endif
-#if __ANDROID__
-                Xamarin.Forms.DependencyService.Register<IEmailService>();
-                DependencyService.Get<IEmailService>().StartEmailActivity(emailMessage);
-#endif
-                contactUsBtn.IsEnabled = true;
+                ContactUs();
             };
             logOutBtn.Clicked += async (sender, e) =>
             {
-                bool logout = await DisplayAlert("Logout", "Are you sure you want to log out " + user.Email + "?", "Yes, I'll be back friend.", "No, I'll stay!");
-                if (logout)
-                {
-                    LogOut();
-                }
+                await LogOutClick();
             };
             settingsBtn.Clicked += async (sender, e) =>
             {
-                string[] settings = { "Change Password" };
-                string settingSelection = await DisplayActionSheet("Settings", "Cancel", null, settings);
-                if (settingSelection.Equals("Change Password"))
-                {
-                    await Navigation.PushModalAsync(new ChangePasswordPage(user));
-                }
+                await Settings();
             };
 
             outerGrid.Children.Add(innerGrid, 0, 0);
@@ -383,11 +479,11 @@ namespace MahechaBJJ.Views
 
                 if (_baseViewModel.User == null)
                 {
-                    try 
+                    try
                     {
-                        user = await _baseViewModel.FindUserByIdAsync(Constants.FINDUSER, account.Properties["Id"]); 
+                        user = await _baseViewModel.FindUserByIdAsync(Constants.FINDUSER, account.Properties["Id"]);
                     }
-                    catch (KeyNotFoundException ex) 
+                    catch (KeyNotFoundException ex)
                     {
                         Console.WriteLine(ex.StackTrace);
                         await DisplayAlert("Unknown Error", "There has been an unknown error, please sign in again.", "Ok");
@@ -408,15 +504,6 @@ namespace MahechaBJJ.Views
                     userCredentialStack.Children.Add(beltTextLbl);
                     userCredentialStack.Children.Add(emailLbl);
                     userCredentialStack.Children.Add(emailTextLbl);
-#endif
-#if __ANDROID__
-                nameLbl.Text = $"{nameLbl.Text}: {user.Name}";
-                emailLbl.Text = $"{emailLbl.Text}: {user.Email}";
-                beltLbl.Text = $"{beltLbl.Text}: {user.Belt}";
-                userCredentialStack.Children.Add(nameLbl);
-                userCredentialStack.Children.Add(beltLbl);
-                userCredentialStack.Children.Add(emailLbl);
-#endif
 
                     //Building Grid
                     innerGrid.Children.Clear();
@@ -426,6 +513,27 @@ namespace MahechaBJJ.Views
                     innerGrid.Children.Add(contactUsBtn, 0, 4);
                     innerGrid.Children.Add(settingsBtn, 0, 5);
                     innerGrid.Children.Add(logOutBtn, 0, 6);
+#endif
+#if __ANDROID__
+                    androidNameLbl.Text = $"{androidNameLbl.Text} {user.Name}";
+                    androidEmailLbl.Text = $"{androidEmailLbl.Text} {user.Email}";
+                    androidBeltLbl.Text = $"{androidBeltLbl.Text} {user.Belt}";
+                    userCredentialStack.Children.Add(contentViewNameLbl);
+                    userCredentialStack.Children.Add(contentViewEmailLbl);
+                    userCredentialStack.Children.Add(contentViewBeltLbl);
+
+                    //Building Grid
+                    innerGrid.Children.Clear();
+                    //innerGrid.Children.Add(userCredentialStack, 0, 0);
+                    innerGrid.Children.Add(contentViewNameLbl, 0, 0);
+                    innerGrid.Children.Add(contentViewEmailLbl, 0, 1);
+                    innerGrid.Children.Add(contentViewBeltLbl, 0, 2);
+                    //Grid.SetRowSpan(userCredentialStack, 3);
+                    innerGrid.Children.Add(androidPackageBtn.ToView(), 0, 3);
+                    innerGrid.Children.Add(androidContactUsBtn.ToView(), 0, 4);
+                    innerGrid.Children.Add(androidSettingsBtn.ToView(), 0, 5);
+                    innerGrid.Children.Add(androidLogOutBtn.ToView(), 0, 6);
+#endif
                 }
                 else
                 {
@@ -434,6 +542,14 @@ namespace MahechaBJJ.Views
             }
             else
             {
+#if __ANDROID__
+                //Building Grid
+                innerGrid.Children.Clear();
+                innerGrid.Children.Add(androidPackageBtn.ToView(), 0, 1);
+                innerGrid.Children.Add(androidContactUsBtn.ToView(), 0, 3);
+                innerGrid.Children.Add(androidLoginBtn.ToView(), 0, 4);
+                innerGrid.Children.Add(androidCreateAccountBtn.ToView(), 0, 5);
+#endif
 #if __IOS__
                 innerGrid.Children.Clear();
                 innerGrid.Children.Add(packageBtn, 0, 1);
@@ -469,6 +585,40 @@ namespace MahechaBJJ.Views
             else
             {
                 Navigation.PushModalAsync(new PurchasePage(Package.Gi));
+            }
+        }
+
+        private async Task LogOutClick()
+        {
+            bool logout = await DisplayAlert("Logout", "Are you sure you want to log out " + user.Email + "?", "Yes, I'll be back friend.", "No, I'll stay!");
+            if (logout)
+            {
+                LogOut();
+            }
+        }
+
+        private void ContactUs()
+        {
+            contactUsBtn.IsEnabled = false;
+            var emailMessage = new EmailMessage();
+            emailMessage.Subject = "Mahecha BJJ - <Insert Subject Here>";
+#if __IOS__
+                MessagingCenter.Send(this, "Send EMail", emailMessage);
+#endif
+#if __ANDROID__
+            Xamarin.Forms.DependencyService.Register<IEmailService>();
+            DependencyService.Get<IEmailService>().StartEmailActivity(emailMessage);
+#endif
+            contactUsBtn.IsEnabled = true;
+        }
+
+        private async Task Settings() 
+        {
+            string[] settings = { "Change Password" };
+            string settingSelection = await DisplayActionSheet("Settings", "Cancel", null, settings);
+            if (settingSelection.Equals("Change Password"))
+            {
+                await Navigation.PushModalAsync(new ChangePasswordPage(user));
             }
         }
     }
